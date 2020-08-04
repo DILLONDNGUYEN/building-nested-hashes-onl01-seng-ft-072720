301c9c1a01589846ea71f532f81a245220c148ac
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-04 04:08:49 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,12 +2,14 @@
 #your key/value pairs here
 #your key/value pairs here
 #your key/value pairs here
- #your key/value pairs here
+#your key/value pairs here
+
 def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        
+        :name "Lord Montague"
+        :age "53"
         },
       :matriarch => {
         
