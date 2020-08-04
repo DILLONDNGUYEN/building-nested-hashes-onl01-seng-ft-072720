3e9e328b250bebeede743940a136ba81ec972c49
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-04 04:13:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -24,10 +24,12 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        
+        :name "Lord Capulet"
+        :age "50"
         },
       :matriarch => {
-       
+        :name "Lady Capulet"
+        :age "51"
         },
       :heroine => {
         #your key/value pairs here
